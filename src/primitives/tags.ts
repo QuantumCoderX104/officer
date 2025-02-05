28f52f3186bf54fe2061a12014d96c062de0e866
QuantumCoderX104 <nicholascadet@mail.com>
2025-02-05 19:47:40 +0000

    Fix configuration - refactoring code

--- src/primitives/tags.ts
+++ src/primitives/tags.ts
@@ -1,8 +1,8 @@
 import { TagColorMode, TagTypes } from '@components/Tags/types';
 import { colorGuide } from '@primitives';
 
-export let getTagColor = (type: TagTypes, mode: TagColorMode) => {
-    let colorConfig = colorGuide[mode == 'light' ? 'lightComponents' : 'darkComponents'].tags;
+export const getTagColor = (type: TagTypes, mode: TagColorMode) => {
+    const colorConfig = colorGuide[mode == 'light' ? 'lightComponents' : 'darkComponents'].tags;
     switch (type) {
         case 'success':
             return colorConfig.success;
