b6115ab9e808c0ec9207d0a255f739b3721e13a5
QuantumCoderX104 <nicholascadet@mail.com>
2025-03-13 07:04:17 +0000

    Improve configuration - removing deprecated function

--- src/components/Typography/styles.ts
+++ src/components/Typography/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 import { generateTextStyle } from '@utils';
 import { TypographyProps } from './types';
 
-export let TypographyWrapper = styled.div<TypographyProps>`
+export const TypographyWrapper = styled.div<TypographyProps>`
     ${(props) => generateTextStyle(props.fontType, props.fontSize, props.fontWeight, props.color)};
     ${(props) =>
         props.overflow &&
@@ -22,7 +22,7 @@ export let TypographyWrapper = styled.div<TypographyProps>`
         `};
 `;
 
-export let ParagraphWrapper = styled.p<TypographyProps>`
+export const ParagraphWrapper = styled.p<TypographyProps>`
     ${(props) => generateTextStyle(props.fontType, props.fontSize, props.fontWeight, props.color)};
     ${(props) =>
         props.overflow &&
@@ -42,7 +42,7 @@ export let ParagraphWrapper = styled.p<TypographyProps>`
         `};
 `;
 
-export let SpanWrapper = styled.span<TypographyProps>`
+export const SpanWrapper = styled.span<TypographyProps>`
     ${(props) => generateTextStyle(props.fontType, props.fontSize, props.fontWeight, props.color)};
     ${(props) =>
         props.overflow &&
